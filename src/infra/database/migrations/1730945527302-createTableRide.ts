a7c754abe4f11dae1cf0061a8363b207bfcf2b08
Ericles-Millere <ericlesmiller15@gmail.com>
2024-11-07 01:21:29 -0300

    feat: create ride

--- src/infra/database/migrations/1730945527302-createTableRide.ts
+++ src/infra/database/migrations/1730945527302-createTableRide.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm';
 
-export class CreateTableRides1730945527302 implements MigrationInterface {
+export class CreateTableRide1730945527302 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
@@ -57,7 +57,7 @@ export class CreateTableRides1730945527302 implements MigrationInterface {
           {
             name: 'driverId',
             type: 'uuid',
-            isPrimary: true,
+            isNullable: true,
           },
         ],
       }),
